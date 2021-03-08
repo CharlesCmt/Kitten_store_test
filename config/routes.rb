63d20f733046f8567391fca04964aad7745e17af
Youssef <annabiyoussef1986@gmail.com>
2021-03-08 17:40:08 +0100

    devise installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'static_pages/index'
+  get 'static_pages/secret'
+  devise_for :users
   root 'items#index'
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
