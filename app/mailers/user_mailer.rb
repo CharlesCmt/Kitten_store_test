19b11c43263128509c6abcfbc164bdcbea0af9cd
Janainaf <figueira.janaina@gmail.com>
2021-03-11 09:16:50 +0100

    drft email text

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,16 @@
 class UserMailer < ApplicationMailer
-end
+    default from: 'no-reply@kittenshop.fr'
+ 
+    def order_email(order)
+      #on récupère l'instance user pour ensuite pouvoir la passer à la view en @order
+      @order = order 
+  
+      #on définit une variable @url qu'on utilisera dans la view d’e-mail
+      @url  = 'http://kittenshop.fr/login' 
+  
+      # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+      mail(to: @user.email, 
+        subject: "Confirmation de votre commande Kitten Shop",
+      )     
+    end
+  end
