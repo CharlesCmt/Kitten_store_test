bccd5fd84cb74849e3b7af342907b475b5233008
Janainaf <figueira.janaina@gmail.com>
2021-03-11 09:21:24 +0100

    added after create to model Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,11 @@ class Order < ApplicationRecord
   has_many :order_items
   before_save :set_subtotal
 
+  after_create :send_order_email
+
+  def send_order_email
+    UserMailer.order_email(self).deliver_now!
+  end
 
   def subtotal
     order_items.collect{|order_item| order_item.valid? ? order_item.unit_price*order_item.quantity : 0}.sum
